419351498f6d2a52aefffd784b3b5e4a62e4860d
sumittr <sumittr@sumittrs-mbp.guest.corp.microsoft.com>
2016-12-05 11:21:46 +0530

    Initial Commit

---
+++ CocoaPods_packagetest/CocoaPods_packagetest.h
@@ -0,0 +1,19 @@
+//
+//  CocoaPods_packagetest.h
+//  CocoaPods_packagetest
+//
+//  Created by sumittr on 05/12/16.
+//  Copyright © 2016 sumittr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CocoaPods_packagetest.
+FOUNDATION_EXPORT double CocoaPods_packagetestVersionNumber;
+
+//! Project version string for CocoaPods_packagetest.
+FOUNDATION_EXPORT const unsigned char CocoaPods_packagetestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaPods_packagetest/PublicHeader.h>
+
+
